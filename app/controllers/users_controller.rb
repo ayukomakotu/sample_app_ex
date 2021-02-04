266cb7fc6a1539b5b801ca4a1d183bbdf1abc791
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-02-04 22:01:12 +0900

    finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,34 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  
+  # POST /users (+ params)
+  def create
+    
+    # (@user + given params).save
+    @user = User.create(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Sample App!"
+      redirect_to @user 
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id) 
+      # redirect_to user_path(1) => /users/1 
+      # GET "users/#{@user.id}"
+    else
+      render 'new'
+    end
+  end
+  
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
+
