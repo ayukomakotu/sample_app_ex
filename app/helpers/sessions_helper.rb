dc355c3099c1744f8b1a399b78f0355039af5ca4
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-02-08 00:39:36 +0900

    チュートリアル動画８章前編終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,27 @@
 module SessionsHelper
+    # 渡されたユーザーでログインする
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    # 現在ログイン中のユーザーを返す（いる場合）
+    def current_user
+        #DBへの問い合わせを可能な限り小さくしたい
+        if session[:user_id]
+            #User.find_by(id: session[:user_id])
+            # a ||= User.find...  =>  a = a or User.find...
+            #左が実行されれば右も実行する　　=>  左　||　右
+            @current_user ||= User.find_by(id: session[:user_id])
+            #if @current_user.nil?
+            #    @current_user = User.find_by(id: session[:user_id])
+            #else
+            #    return @current_user
+            #end  の省略
+        end
+    end
+    
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+        !current_user.nil?
+    end
 end
