efe3c1fe633c9e560a6729b57a14374612a13ebe
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-03-03 23:20:38 +0900

    finish list111.30

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,9 +24,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     #ここでテストしたい時もある
     follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.blank?
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert_not flash.blank?
+    # assert is_logged_in?
   end
 
 
