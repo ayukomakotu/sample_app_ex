e8e7853823a8d344398cb4b7fdee1af07f95ab5d
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-03-09 22:22:34 +0900

    finish ch11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   #GET /users
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
   
   # GET /users/new
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
