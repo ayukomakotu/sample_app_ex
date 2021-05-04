0005f80ef19a795a514ceb8ffe9fa2fef51a4bba
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-05-04 11:39:42 +0900

    application.js修正 error５個warning継続中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 import "bootstrap"
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,3 +18,9 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+//修正　by update rails6.0.3からrails6.1.3.6
