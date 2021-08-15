fe536d31e948842c3e373bdc2aef34273e92c0a0
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-08-16 00:25:48 +0900

    in_reply_to columns add default false

--- db/migrate/20210815142552_add_in_reply_to_to_microposts.rb
+++ db/migrate/20210815142552_add_in_reply_to_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddInReplyToToMicroposts < ActiveRecord::Migration[6.1]
   def change
-    add_column :microposts, :in_reply_to, :boolean
+    add_column :microposts, :in_reply_to, :boolean, default: false
   end
 end
