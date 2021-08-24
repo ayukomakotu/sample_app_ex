7ef1219f0057ac58006d1e212fcea116e2ac67e8
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-08-24 23:34:50 +0900

    migration修正

---
+++ .history/app/controllers/microposts_controller_20210818234321.rb
@@ -0,0 +1,42 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    @users = User.all
+    @reply_user = []
+    @users.each do |user|
+      if @micropost.content.include?("@#{user}")
+        @reply_user.push(user)
+        @micropost = true
+      end
+    debugger
+    end
+
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_back(fallback_location: root_url)
+  end
+
+
+private
+  def micropost_params
+    params.require(:micropost).permit(:content, :image)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_122558) do
+ActiveRecord::Schema.define(version: 2021_08_22_150631) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,16 +35,17 @@ ActiveRecord::Schema.define(version: 2021_05_03_122558) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "in_reply_to"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
