26bb57acb0e1590f6b722a5b0f712ad6cc938c11
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-08-27 00:27:31 +0900

    add Reply model micropost controller編集 create action内in_reply_to格納機構実装途中　次回varidation等実装

--- db/migrate/20210826130421_create_replies.rb
+++ db/migrate/20210826130421_create_replies.rb
@@ -2,8 +2,10 @@ class CreateReplies < ActiveRecord::Migration[6.1]
   def change
     create_table :replies do |t|
       t.integer :in_reply_to
+      t.references :micropost, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :replies, [:micropost_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_143733) do
+ActiveRecord::Schema.define(version: 2021_08_26_130421) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,8 +62,11 @@ ActiveRecord::Schema.define(version: 2021_08_24_143733) do
 
   create_table "replies", force: :cascade do |t|
     t.integer "in_reply_to"
+    t.integer "micropost_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["micropost_id", "created_at"], name: "index_replies_on_micropost_id_and_created_at"
+    t.index ["micropost_id"], name: "index_replies_on_micropost_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -85,4 +88,5 @@ ActiveRecord::Schema.define(version: 2021_08_24_143733) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "microposts", "users"
+  add_foreign_key "replies", "microposts"
 end
