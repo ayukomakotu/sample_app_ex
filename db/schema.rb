9b296246115d102d42105bf67bb7ed1ea1ecf5d6
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-11-08 23:35:00 +0900

    users_tableにfollow_notifyカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_152300) do
+ActiveRecord::Schema.define(version: 2021_11_08_142225) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2021_10_09_152300) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean "follow_notify", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
