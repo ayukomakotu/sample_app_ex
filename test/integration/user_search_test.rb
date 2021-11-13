ecf872fe38d43a135c7590f788c4faac86a62ffd
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-11-14 02:11:05 +0900

    検索条件に合致したuserが存在しない場合の処理を追加&test

--- test/integration/user_search_test.rb
+++ test/integration/user_search_test.rb
@@ -9,11 +9,23 @@ class UserSearchTest < ActionDispatch::IntegrationTest
 
   test "invalid user search" do
     log_in_as(@user)
+    #nameが空白
     get users_path
     get search_users_path, params: {search: {name: "   "}}
     assert_template 'users/index'
+    #idが空白
+    get users_path
     get search_users_path, params: {search: {id: "  "}}
     assert_template 'users/index'
+    #存在しないユーザー
+    get users_path
+    get search_users_path, params: {search: {name: "nobody"}}
+    assert_template 'users/search'
+    assert_match "User not found", response.body
+    get users_path
+    get search_users_path, params: {search: {id: 0}}
+    assert_template 'users/search'
+    assert_match "User not found", response.body
   end
   
   test "user search for name" do
