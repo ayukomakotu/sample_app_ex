beb9bc1402b5da30bba0f15d5eed5233fbaa13e5
山根　直也 <yamane@yamanenaoyanoMacBook-Pro.local>
2021-11-14 01:45:57 +0900

    test追加

--- test/integration/user_search_test.rb
+++ test/integration/user_search_test.rb
@@ -12,9 +12,11 @@ class UserSearchTest < ActionDispatch::IntegrationTest
     get users_path
     get search_users_path, params: {search: {name: "   "}}
     assert_template 'users/index'
+    get search_users_path, params: {search: {id: "  "}}
+    assert_template 'users/index'
   end
   
-  test "user search interface" do
+  test "user search for name" do
     log_in_as(@user)
     get users_path
     get search_users_path, params: {search: {name: @searched.name}} #完全一致
@@ -22,7 +24,16 @@ class UserSearchTest < ActionDispatch::IntegrationTest
     assert_match @searched.name, response.body 
     get users_path
     get search_users_path, params: {search: {name: "Archer"}} #部分一致
+    assert_template 'users/search'
     assert_match @searched.name, response.body
     assert_match @other_searched.name, response.body
+  end
+
+  test "user search for id" do
+    log_in_as(@user)
+    get users_path
+    get search_users_path, params: {search: {id: @searched.id}}
+    assert_template 'users/search'
+    assert_match @searched.name, response.body
   end
 end
