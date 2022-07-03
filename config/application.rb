5527dea4891ecad103bbd08f94f04203f9960551
ayukomakotu <yamamesan708@gmail.com>
2022-07-03 16:37:00 +0900

    improve application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.active_record.legacy_connection_handling = false
 
     # Configuration for the application, engines, and railties goes here.
