d935436a3a3ec7b0b6d05b27f637dcd9726b8cb8
ayukomakotu <yamamesan708@gmail.com>
2022-07-10 11:47:27 +0900

    link_to delete修正 turboに合わせて

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    redirect_to users_url, status: :see_other
   end
 
   def following
